e4ac221696b157bfe5d776a214dc54ed5945857a
yihisab-debug <yi.hisab@gmail.com>
2026-04-15 17:38:56 +0500

    Update appointment.dart

--- lib/models/appointment.dart
+++ lib/models/appointment.dart
@@ -1,58 +1,31 @@
 class Appointment {
   final String id;
-  final String userId;
-  final String doctorId;
-  final String doctorName;       
-  final String doctorSpec;      
-  final String slotId;
-  final String date;            
-  final String startTime;   
+  final String name;
+  final String specialization;
+  final DateTime date;
+  final String startTime;
   final String endTime;
-  String status;
-
-  final String createdAt;
+  final bool isBooked;
 
   Appointment({
     required this.id,
-    required this.userId,
-    required this.doctorId,
-    this.doctorName = '',
-    this.doctorSpec = '',
-    required this.slotId,
-    this.date = '',
-    this.startTime = '',
-    this.endTime = '',
-    required this.status,
-    required this.createdAt,
+    required this.name,
+    required this.specialization,
+    required this.date,
+    required this.startTime,
+    required this.endTime,
+    required this.isBooked,
   });
 
   factory Appointment.fromJson(Map<String, dynamic> json) {
     return Appointment(
-      id:         json['id'].toString(),
-      userId:     json['userId'] ?? '',
-      doctorId:   json['doctorId'].toString(),
-      doctorName: json['doctorName'] ?? '',
-      doctorSpec: json['doctorSpec'] ?? '',
-      slotId:     json['slotId'].toString(),
-      date:       json['date'] ?? '',
-      startTime:  json['startTime'] ?? '',
-      endTime:    json['endTime'] ?? '',
-      status:     json['status'] ?? 'confirmed',
-      createdAt:  json['createdAt'] ?? '',
+      id: json['id'],
+      name: json['name'],
+      specialization: json['specialization'],
+      date: DateTime.parse(json['date']), // 🔥 ключевая строка
+      startTime: json['startTime'],
+      endTime: json['endTime'],
+      isBooked: json['isBooked'],
     );
   }
-
-  Map<String, dynamic> toJson() => {
-    'id':         id,
-    'userId':     userId,
-    'doctorId':   doctorId,
-    'doctorName': doctorName,
-    'doctorSpec': doctorSpec,
-    'slotId':     slotId,
-    'date':       date,
-    'startTime':  startTime,
-    'endTime':    endTime,
-    'status':     status,
-    'createdAt':  createdAt,
-  };
 }
